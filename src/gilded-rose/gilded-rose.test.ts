41412d59e0c8f3e2741ba18dff68fa8c8099bba3
German Delgado <germandg6@gmail.com>
2024-11-26 16:41:11 +0000

    test: add test for Sulfuras items and standart items

---
+++ src/gilded-rose/gilded-rose.test.ts
@@ -0,0 +1,55 @@
+import { GildedRose, Item } from './gilded-rose';
+
+describe('Gilded Rose', () => {
+  it('Should decrease by one the sellIn value after update', () => {
+    const sellIn = 10;
+    const item = new Item('item', sellIn, 10);
+
+    new GildedRose([item]).updateQuality();
+
+    expect(item.sellIn).toBe(sellIn - 1);
+  });
+  it('Should decrease by one the quality after update', () => {
+    const quality = 20;
+    const item = new Item('item', 10, quality);
+
+    new GildedRose([item]).updateQuality();
+
+    expect(item.quality).toBe(quality - 1);
+  });
+  it('Should decrease double quality when sell by date has passed', () => {
+    const sellIn = 0;
+    const quality = 20;
+    const item = new Item('item', sellIn, quality);
+
+    new GildedRose([item]).updateQuality();
+
+    expect(item.quality).toBe(quality - 2);
+  });
+
+  describe('Sulfuras', () => {
+    it('never change', () => {
+      const item = new Item('Sulfuras', 20, 25);
+
+      new GildedRose([item]).updateQuality();
+
+      expect(item).toBe(item);
+    });
+  });
+
+  describe('Back Stage Passes', () => {
+    it('Should increase quality by 2 for sellIn values of less than 10', () => {
+      const sellIn = 10;
+      const quality = 30;
+      const item = new Item(
+        'Backstage passes to a TAFKAL80ETC concert',
+        sellIn,
+        quality
+      );
+
+      new GildedRose([item]).updateQuality();
+
+      expect(item.quality).toBe(quality + 2);
+    });
+  });
+});
