26b88cd5afc8de6cec9baf658cafbb9429f5b691
German Delgado <germandg6@gmail.com>
2024-11-27 17:09:56 +0000

    refactor: refactor tests

--- src/gilded-rose/gilded-rose.test.ts
+++ src/gilded-rose/gilded-rose.test.ts
@@ -1,55 +1,66 @@
-import { GildedRose, Item } from './gilded-rose';
-
+import { BackstagePasses } from './backstage-passes';
+import { GildedRose } from './gilded-rose';
+import { ItemQuality } from './item-quality';
+import { ItemSellIn } from './item-sellIn';
+import { StandartItem } from './standar-item';
+import { Sulfuras } from './sulfuras';
 describe('Gilded Rose', () => {
   it('Should decrease by one the sellIn value after update', () => {
-    const sellIn = 10;
-    const item = new Item('item', sellIn, 10);
+    const sellIn = new ItemSellIn(10);
+    const item = new StandartItem('item', sellIn, new ItemQuality(10));
 
     new GildedRose([item]).updateQuality();
 
-    expect(item.sellIn).toBe(sellIn - 1);
+    const expectedSellIn = new ItemSellIn(9);
+    expect(item.sellIn).toEqual(expectedSellIn);
   });
   it('Should decrease by one the quality after update', () => {
-    const quality = 20;
-    const item = new Item('item', 10, quality);
+    const quality = new ItemQuality(20);
+    const item = new StandartItem('item', new ItemSellIn(10), quality);
 
     new GildedRose([item]).updateQuality();
 
-    expect(item.quality).toBe(quality - 1);
+    const expectedQuality = new ItemQuality(19);
+    expect(item.quality).toEqual(expectedQuality);
   });
   it('Should decrease double quality when sell by date has passed', () => {
-    const sellIn = 0;
-    const quality = 20;
-    const item = new Item('item', sellIn, quality);
+    const sellIn = new ItemSellIn(0);
+    const quality = new ItemQuality(20);
+    const item = new StandartItem('item', sellIn, quality);
 
     new GildedRose([item]).updateQuality();
 
-    expect(item.quality).toBe(quality - 2);
+    const expectedQuality = new ItemQuality(18);
+    expect(item.quality).toEqual(expectedQuality);
   });
   it('The quality of an item never can be negative', () => {
-    const quality = 0;
-    const item = new Item('item', 0, quality);
+    const quality = new ItemQuality(0);
+    const item = new StandartItem('item', new ItemSellIn(0), quality);
 
     new GildedRose([item]).updateQuality();
 
-    expect(item.quality).toBe(quality);
+    expect(item.quality).toEqual(quality);
   });
 
   describe('Sulfuras', () => {
     it('never change', () => {
-      const item = new Item('Sulfuras', 20, 25);
+      const item = new Sulfuras(
+        'Sulfuras',
+        new ItemSellIn(20),
+        new ItemQuality(25)
+      );
 
       new GildedRose([item]).updateQuality();
 
-      expect(item).toBe(item);
+      expect(item).toEqual(item);
     });
   });
 
   describe('Back Stage Passes', () => {
     it('Should increase quality by 2 for sellIn values of less than 10', () => {
-      const sellIn = 10;
-      const quality = 30;
-      const item = new Item(
+      const sellIn = new ItemSellIn(10);
+      const quality = new ItemQuality(30);
+      const item = new BackstagePasses(
         'Backstage passes to a TAFKAL80ETC concert',
         sellIn,
         quality
@@ -57,13 +68,14 @@ describe('Gilded Rose', () => {
 
       new GildedRose([item]).updateQuality();
 
-      expect(item.quality).toBe(quality + 2);
+      const expectedQuality = new ItemQuality(32);
+      expect(item.quality).toEqual(expectedQuality);
     });
 
     it('Should increase quality by 3 for sellIn values of less than 5', () => {
-      const sellIn = 5;
-      const quality = 30;
-      const item = new Item(
+      const sellIn = new ItemSellIn(5);
+      const quality = new ItemQuality(30);
+      const item = new BackstagePasses(
         'Backstage passes to a TAFKAL80ETC concert',
         sellIn,
         quality
@@ -71,12 +83,13 @@ describe('Gilded Rose', () => {
 
       new GildedRose([item]).updateQuality();
 
-      expect(item.quality).toBe(quality + 3);
+      const expectedQuality = new ItemQuality(33);
+      expect(item.quality).toEqual(expectedQuality);
     });
     it('Should quality drop to 0 after the concert', () => {
-      const sellIn = 0;
-      const quality = 30;
-      const item = new Item(
+      const sellIn = new ItemSellIn(0);
+      const quality = new ItemQuality(30);
+      const item = new BackstagePasses(
         'Backstage passes to a TAFKAL80ETC concert',
         sellIn,
         quality
@@ -84,7 +97,8 @@ describe('Gilded Rose', () => {
 
       new GildedRose([item]).updateQuality();
 
-      expect(item.quality).toBe(0);
+      const expectedQuality = new ItemQuality(0);
+      expect(item.quality).toEqual(expectedQuality);
     });
   });
 });
