becdba16574b6a9ae5e402e29f2f1310a02f7889
German Delgado <germandg6@gmail.com>
2025-03-17 13:20:47 +0000

    refactor: reorganize test

--- src/prime-factors/prime-factors.test.ts
+++ src/prime-factors/prime-factors.test.ts
@@ -1,18 +1,14 @@
-import { getPrimeFactors } from './prime-factors';
+import { getPrimeFactorsFor } from './prime-factors';
 
 describe('Prime factors', () => {
-  it('finds the prime composition of the given number', () => {
-    expect(getPrimeFactors(2)).toEqual([2]);
+  it('knows what is a prime number', () => {
+    expect(getPrimeFactorsFor(2)).toEqual([2]);
+    expect(getPrimeFactorsFor(3)).toEqual([3]);
+  });
+  it('produces the same result to multiply the numbers in the output list', () => {
+    expect(getPrimeFactorsFor(2 * 2 * 2)).toEqual([2, 2, 2]);
+  });
+  it('orders the prime factors to the smallest to the biggest output list', () => {
+    expect(getPrimeFactorsFor(5 * 7 * 11 * 3)).toEqual([3, 5, 7, 11]);
   });
 });
-
-/*
-2 -> [2]
-2 * 2 -> [2,2]
-2 * 2 * 2 -> [2,2,2]
-3 -> [3]
-3 * 3 -> [3,3]
-3 * 2 -> [2,3]
-5 * 5 -> [5,5]
-5 * 7 * 11 * 3 -> [3,5,7,11]
-*/
