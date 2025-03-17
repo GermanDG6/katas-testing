d505f8d9561bb554192144e6dcb1f2aebe0d68c4
German Delgado <germandg6@gmail.com>
2025-03-17 13:28:29 +0000

    refactor: flip condition, extract method

--- src/prime-factors/prime-factors.ts
+++ src/prime-factors/prime-factors.ts
@@ -1,4 +1,11 @@
-export function getPrimeFactors(number: number) {
-  number;
-  return [2];
+export function getPrimeFactorsFor(number: number) {
+  const prime = getSmallestPrime(number);
+  const reminder = number / prime;
+  if (reminder <= 1) return [prime];
+  return [prime].concat(getPrimeFactorsFor(reminder));
+}
+function getSmallestPrime(number: number) {
+  let factor = 2;
+  while (number % factor !== 0) ++factor;
+  return factor;
 }
