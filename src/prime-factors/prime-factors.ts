dbc3675aefd4d498869c7af5185f4d1586ea9d44
German Delgado <germandg6@gmail.com>
2025-03-17 13:45:02 +0000

    refactor: extract functions

--- src/prime-factors/prime-factors.ts
+++ src/prime-factors/prime-factors.ts
@@ -1,5 +1,12 @@
 export function getPrimeFactorsFor(number: number) {
+  checkforPositiveNumbers(number);
+  return primeFactors(number);
+}
+function checkforPositiveNumbers(number: number) {
   if (number < 1) throw new Error('only allow positive numbers');
+}
+
+function primeFactors(number: number) {
   const prime = getSmallestPrime(number);
   const reminder = number / prime;
   if (reminder <= 1) return [prime];
