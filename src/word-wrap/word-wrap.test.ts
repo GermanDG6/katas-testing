0039e87acbce2a9908338686278c499526b54f79
German Delgado <germandg6@gmail.com>
2025-03-25 14:10:31 +0000

    feat: wrap text by space and add nullish text and invalid index conditions

--- src/word-wrap/word-wrap.test.ts
+++ src/word-wrap/word-wrap.test.ts
@@ -6,5 +6,11 @@ describe('The WordWrap', () => {
     expect(wordWrap('longword', 4)).toBe('long\nword');
     expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
     expect(wordWrap('abc def', 4)).toBe('abc\ndef');
+    expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
+    expect(wordWrap(' abcdef', 4)).toBe('\nabcd\nef');
+    expect(wordWrap(null, 4)).toBe('');
+    expect(() => wordWrap('hello', -4)).toThrow(
+      'A negative column width is not allowed'
+    );
   });
 });
--- src/word-wrap/word-wrap.ts
+++ src/word-wrap/word-wrap.ts
@@ -1,6 +1,25 @@
 export function wordWrap(text: string, columnWidth: number) {
+  if (columnWidth < 0)
+    throw new Error('A negative column width is not allowed');
+  if (text == null) return '';
   if (text.length <= columnWidth) return text;
-  const wrapedText = text.substring(0, columnWidth);
-  const unwrapedText = text.substring(columnWidth);
-  return wrapedText + '\n' + wordWrap(unwrapedText, columnWidth);
+
+  const wrappedTextIndex = getWrappedTextIndex(text, columnWidth);
+  const unwrappedTextIndex = getUnwrappedTextIndex(text, columnWidth);
+
+  const wrappedText = text.substring(0, wrappedTextIndex).concat('\n');
+  const unwrappedText = text.substring(unwrappedTextIndex);
+
+  return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
+}
+function getUnwrappedTextIndex(text: string, columnWidth: number) {
+  const shallWrapBySpace =
+    text.indexOf(' ') > -1 && text.indexOf(' ') < columnWidth;
+  return shallWrapBySpace ? text.indexOf(' ') + 1 : columnWidth;
+}
+
+function getWrappedTextIndex(text: string, columnWidth: number) {
+  const shallWrapBySpace =
+    text.indexOf(' ') > -1 && text.indexOf(' ') < columnWidth;
+  return shallWrapBySpace ? text.indexOf(' ') : columnWidth;
 }
