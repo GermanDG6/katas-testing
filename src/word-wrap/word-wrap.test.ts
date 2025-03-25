81b8c63a34cda9c401a0c28030a57959f51e7c69
German Delgado <germandg6@gmail.com>
2025-03-25 18:13:01 +0000

    refactor: extract column-width value object

--- src/word-wrap/word-wrap.test.ts
+++ src/word-wrap/word-wrap.test.ts
@@ -1,15 +1,19 @@
-import { wordWrap } from './word-wrap';
+import { ColumnWidth, wordWrap } from './word-wrap';
 describe('The WordWrap', () => {
   it('makes every single line of text fit column width', () => {
-    expect(wordWrap('', 5)).toBe('');
-    expect(wordWrap('hello', 5)).toBe('hello');
-    expect(wordWrap('longword', 4)).toBe('long\nword');
-    expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd');
-    expect(wordWrap('abc def', 4)).toBe('abc\ndef');
-    expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
-    expect(wordWrap(' abcdef', 4)).toBe('\nabcd\nef');
-    expect(wordWrap(null, 4)).toBe('');
-    expect(() => wordWrap('hello', -4)).toThrow(
+    expect(wordWrap('', ColumnWidth.create(5))).toBe('');
+    expect(wordWrap('hello', ColumnWidth.create(5))).toBe('hello');
+    expect(wordWrap('longword', ColumnWidth.create(4))).toBe('long\nword');
+    expect(wordWrap('reallylongword', ColumnWidth.create(4))).toBe(
+      'real\nlylo\nngwo\nrd'
+    );
+    expect(wordWrap('abc def', ColumnWidth.create(4))).toBe('abc\ndef');
+    expect(wordWrap('abc def ghi', ColumnWidth.create(4))).toBe(
+      'abc\ndef\nghi'
+    );
+    expect(wordWrap(' abcdef', ColumnWidth.create(4))).toBe('\nabcd\nef');
+    expect(wordWrap(null, ColumnWidth.create(4))).toBe('');
+    expect(() => wordWrap('hello', ColumnWidth.create(-4))).toThrow(
       'A negative column width is not allowed'
     );
   });
--- src/word-wrap/word-wrap.ts
+++ src/word-wrap/word-wrap.ts
@@ -1,11 +1,9 @@
-export function wordWrap(text: string, columnWidth: number) {
-  if (columnWidth < 0)
-    throw new Error('A negative column width is not allowed');
+export function wordWrap(text: string, columnWidth: ColumnWidth) {
   if (text == null) return '';
-  if (text.length <= columnWidth) return text;
+  if (text.length <= columnWidth.value()) return text;
 
-  const wrappedTextIndex = getWrappedTextIndex(text, columnWidth);
-  const unwrappedTextIndex = getUnwrappedTextIndex(text, columnWidth);
+  const wrappedTextIndex = getWrappedTextIndex(text, columnWidth.value());
+  const unwrappedTextIndex = getUnwrappedTextIndex(text, columnWidth.value());
 
   const wrappedText = text.substring(0, wrappedTextIndex).concat('\n');
   const unwrappedText = text.substring(unwrappedTextIndex);
@@ -22,4 +20,14 @@ function getWrappedTextIndex(text: string, columnWidth: number) {
   const shallWrapBySpace =
     text.indexOf(' ') > -1 && text.indexOf(' ') < columnWidth;
   return shallWrapBySpace ? text.indexOf(' ') : columnWidth;
+}
+export class ColumnWidth {
+  private constructor(private readonly width: number) {}
+  static create(width: number) {
+    if (width < 0) throw new Error('A negative column width is not allowed');
+    return new ColumnWidth(width);
+  }
+  value() {
+    return this.width;
+  }
 }
