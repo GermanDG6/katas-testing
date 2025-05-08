d9c9c924323bc4fbe401cfbd44988a14591f3ac6
German Delgado <germandg6@gmail.com>
2025-05-08 18:28:35 +0100

    feat: print a statement of an account of severals transactions trougth the console

--- src/banking-kata/test/unit/statement-printer.test.ts
+++ src/banking-kata/test/unit/statement-printer.test.ts
@@ -1,14 +1,37 @@
 import { Console } from '../../console';
 import { StatementPrinter } from '../../statement-printer';
+import { Transaction } from '../../transaction';
 
 describe('The statement printer', () => {
+  let console;
+  let consoleSpy;
+  let statementPrinter;
+  beforeEach(() => {
+    console = new Console();
+    consoleSpy = jest.spyOn(console, 'log');
+    statementPrinter = new StatementPrinter(console);
+  });
   it('always print the header througth the console', () => {
-    const console = new Console();
-    const consoleSpy = jest.spyOn(console, 'log');
-    const statementPrinter = new StatementPrinter(console);
-
     statementPrinter.print([]);
 
     expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+  });
+  it('prints a statement of account including a given transaction througth the console', () => {
+    const transaction = new Transaction('10/01/2022', 1000);
+
+    statementPrinter.print([transaction]);
+
+    expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+    expect(consoleSpy).toHaveBeenCalledWith('10/01/2022 | 1000.00 | 1000.00');
+  });
+  it('prints a statement of account includes several transactions througth the console', () => {
+    statementPrinter.print([
+      new Transaction('13/01/2022', -500),
+      new Transaction('10/01/2022', 1000),
+    ]);
+
+    expect(consoleSpy).toHaveBeenCalledWith('Date | Amount | Balance');
+    expect(consoleSpy).toHaveBeenCalledWith('10/01/2022 | 1000.00 | 1000.00');
+    expect(consoleSpy).toHaveBeenCalledWith('13/01/2022 | -500.00 | 500.00');
   });
 });
