25e2fc5ca9b34ba1cdae597589c36fb520264948
GermanDG <gdelgado@civitfun.com>
2025-10-09 11:27:40 +0100

    green: should calculate a spare and sum the points extra

--- src/bowling/bowling-kata.spec.ts
+++ src/bowling/bowling-kata.spec.ts
@@ -1,15 +1,71 @@
 class BowlingGame {
   private rolls: number[] = [];
+  private readonly maxScorePerFrame = 10;
+
   roll(bowls: number) {
     this.rolls.push(bowls);
   }
 
   calculateTotalScore() {
-    return this.rolls.reduce((acc, current) => {
-      return acc + current;
+    const score = this.frames().reduce(this.calculatesScorePerFrame, {
+      totalScore: 0,
+      frameIndex: 0,
     });
+    return score.totalScore;
+  }
+
+  private calculatesScorePerFrame = ({ totalScore, frameIndex }: Score) => {
+    if (this.isStrike(frameIndex)) {
+      return {
+        totalScore:
+          totalScore + this.maxScorePerFrame + this.strikeBonus(frameIndex),
+        frameIndex: frameIndex + 1,
+      };
+    }
+    if (this.isSpare(frameIndex)) {
+      return {
+        totalScore:
+          totalScore + this.maxScorePerFrame + this.spareBonus(frameIndex),
+        frameIndex: frameIndex + 2,
+      };
+    }
+    return {
+      totalScore: totalScore + this.sumOfBallsInFrame(frameIndex),
+      frameIndex: frameIndex + 2,
+    };
+  };
+
+  private isStrike(frameIndex: number) {
+    return this.rolls[frameIndex] === this.maxScorePerFrame;
+  }
+
+  private strikeBonus(frameIndex: number) {
+    return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2];
+  }
+
+  private isSpare(frameIndex: number) {
+    return this.sumOfBallsInFrame(frameIndex) === this.maxScorePerFrame;
+  }
+
+  private spareBonus(frameIndex: number) {
+    return this.rolls[frameIndex + 2];
+  }
+
+  private sumOfBallsInFrame(frameIndex: number) {
+    return this.rolls[frameIndex] + this.rolls[frameIndex + 1];
+  }
+
+  private frames(): number[] {
+    const numberOfFrames: number = 10;
+
+    return Array.from({ length: numberOfFrames }).map((_, i) => i);
   }
 }
+
+type Score = {
+  totalScore: number;
+  frameIndex: number;
+};
 
 describe('The bowling game', () => {
   let game: BowlingGame;
@@ -29,16 +85,27 @@ describe('The bowling game', () => {
   });
 
   it('should calculate a spare and sum the points extra', () => {
-    game.roll(5);
-    game.roll(5);
+    spareFrame(game);
     game.roll(5);
     rollsMany(17, 0);
+
+    expect(game.calculateTotalScore()).toEqual(20);
+  });
+  it('should calculate a strike and sum the points extra', () => {
+    game.roll(10);
+    game.roll(2);
+    game.roll(3);
+    rollsMany(16, 0);
 
     expect(game.calculateTotalScore()).toEqual(20);
   });
 
   function rollsMany(tries: number = 20, pins: number) {
     Array.from({ length: tries }).forEach(() => game.roll(pins));
+  }
+  function spareFrame(game: BowlingGame) {
+    game.roll(5);
+    game.roll(5);
   }
 });
 
