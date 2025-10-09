ef14b454b0fd52fe229d6ab35456db8f6e59fb54
GermanDG <gdelgado@civitfun.com>
2025-10-09 12:00:05 +0100

    refactor: extract srtike roll

--- src/bowling/bowling-kata.spec.ts
+++ src/bowling/bowling-kata.spec.ts
@@ -2,6 +2,7 @@ import { BowlingGame } from './bowling-game';
 
 describe('The bowling game', () => {
   let game: BowlingGame;
+
   beforeEach(() => {
     game = new BowlingGame();
   });
@@ -11,6 +12,7 @@ describe('The bowling game', () => {
 
     expect(game.calculateTotalScore()).toEqual(0);
   });
+
   it('should give 20 points for a game with all tries with 1 only one knockdown', () => {
     rollsMany(20, 1);
 
@@ -18,20 +20,22 @@ describe('The bowling game', () => {
   });
 
   it('should calculate a spare and sum the points extra', () => {
-    spareFrame(game);
+    spareFrame();
     game.roll(5);
     rollsMany(17, 0);
 
     expect(game.calculateTotalScore()).toEqual(20);
   });
+
   it('should calculate a strike and sum the points extra', () => {
-    game.roll(10);
+    strikeRoll();
     game.roll(2);
     game.roll(3);
     rollsMany(16, 0);
 
     expect(game.calculateTotalScore()).toEqual(20);
   });
+
   it('should calculate a perfect game', () => {
     rollsMany(20, 10);
     game.roll(10);
@@ -43,8 +47,13 @@ describe('The bowling game', () => {
   function rollsMany(tries: number = 20, pins: number) {
     Array.from({ length: tries }).forEach(() => game.roll(pins));
   }
-  function spareFrame(game: BowlingGame) {
+
+  function spareFrame() {
     game.roll(5);
     game.roll(5);
+  }
+
+  function strikeRoll() {
+    game.roll(10);
   }
 });
