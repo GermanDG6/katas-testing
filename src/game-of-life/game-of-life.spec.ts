770017c6cb74cce1ab1df36a83221645624e6934
GermanDG <gdelgado@civitfun.com>
2025-10-14 11:58:56 +0100

    green: dead cells with exactly 3 neighbors are revived

--- src/game-of-life/game-of-life.spec.ts
+++ src/game-of-life/game-of-life.spec.ts
@@ -4,22 +4,52 @@ enum CellStatus {
 }
 
 export class Cell {
-  status: CellStatus;
+  private readonly status: CellStatus;
+  private readonly minOfNeighborsForSurvive = 2;
+
+  private readonly maxOfNeigborsToSurvive = 3;
+
   constructor(status: CellStatus) {
     this.status = status;
   }
 
   regenerate(numberOfNeighbors: number): CellStatus {
+    if (this.hasNeighborsToSurvive(numberOfNeighbors) && this.isAlive())
+      return CellStatus.Alive;
+    if (this.status === CellStatus.Died && numberOfNeighbors === 3)
+      return CellStatus.Alive;
     return CellStatus.Died;
+  }
+
+  private isAlive() {
+    return this.status === CellStatus.Alive;
+  }
+
+  private hasNeighborsToSurvive(numberOfNeighbors: number) {
+    return (
+      numberOfNeighbors === this.minOfNeighborsForSurvive ||
+      numberOfNeighbors === this.maxOfNeigborsToSurvive
+    );
   }
 }
 
-describe('The game of life', () => {
-  it('Living cells with fewer than two neighbors die due to depopulation', () => {
-    const cell = new Cell(CellStatus.Alive);
+describe('In the game of life', () => {
+  it('living cells with fewer than two neighbors die due to depopulation', () => {
+    expect(new Cell(CellStatus.Alive).regenerate(1)).toBe(CellStatus.Died);
+    expect(new Cell(CellStatus.Died).regenerate(1)).toBe(CellStatus.Died);
+  });
 
-    const numberOfNeighbors = 1;
-    const nextGeneration = cell.regenerate(numberOfNeighbors);
-    expect(nextGeneration).toBe(CellStatus.Died);
+  it('living cells with more than three neighbors die from overpopulation', () => {
+    expect(new Cell(CellStatus.Alive).regenerate(4)).toBe(CellStatus.Died);
+    expect(new Cell(CellStatus.Died).regenerate(4)).toBe(CellStatus.Died);
+  });
+
+  it('living cells with two or three neighbors survive.', () => {
+    expect(new Cell(CellStatus.Alive).regenerate(2)).toBe(CellStatus.Alive);
+    expect(new Cell(CellStatus.Died).regenerate(2)).toBe(CellStatus.Died);
+  });
+
+  it('dead cells with exactly 3 neighbors are revived', () => {
+    expect(new Cell(CellStatus.Died).regenerate(3)).toBe(CellStatus.Alive);
   });
 });
